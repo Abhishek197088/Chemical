6a304062203bbfd398509180abeee1e3c0475514
Uttam <your_email@gmail.com>
2026-06-03 12:00:46 +0530

    fix: relocate sqlite database to prisma folder to bundle it successfully on vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const url = process.env.DATABASE_URL || "file:./dev.db";
+const url = process.env.DATABASE_URL || "file:./prisma/dev.db";
 const adapter = new PrismaBetterSqlite3({ url });
 
 export const prisma =
